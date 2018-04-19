d14ef726ddee259d26413c46d9d44e140280e930
Victor Almeida <victordominguesalmeida@gmail.com>
2018-04-18 22:26:25 -0300

    validação class Robo

--- Controller/base.js
+++ Controller/base.js
@@ -1,6 +1,21 @@
-object Robo{
-  var x;
-  var y;
-  var aceleracao;
-  var vm;
+
+class Robo{
+  constructor(x, y, a, vm) {
+  this.x = x;
+  this.y = y;
+  this.a = a;
+  this.vm = vm;
+  }
+}
+
+
+function validaRobo(){
+  var robo = new Robo(10, 20, 2, 2.6);
+  var div = document.getElementById("div1");
+
+  div.innerHTML = "Robo X: " +robo.x+ "<br>" +
+                  "Robo Y: " +robo.y+ "<br>" +
+                  "Aceleração: " +robo.a+ "m/s²<br>" +
+                  "Velocidade Média: " +robo.vm+ "m/s";
+  div.style.background="green";
 }
