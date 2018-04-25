2816b5fba5ff0f5f2dc226fea8f80dd96611ae15
RahLin182 <rafaelfclino@gmail.com>
2018-04-24 22:56:47 -0300

    Tudo novo
    Bora tirar 10

--- README.md
+++
@@ -1,1 +0,0 @@
-# RoboFeiP1
---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var expressLayouts = require('express-ejs-layouts');
+var app = express();
+var port = 3000;
+
+app.set('view engine', 'ejs')    // Setamos que nossa engine será o ejs
+app.use(expressLayouts)          // Definimos que vamos utilizar o express-ejs-layouts na nossa aplicação
+app.use(bodyParser.urlencoded()) // Com essa configuração, vamos conseguir parsear o corpo das requisições
+
+app.get('/', (req, res) => {
+    res.render('pages/home')
+  })
+
+
+app.use(express.static(__dirname + '/public'))
+app.listen(port)
+console.log('Servidor iniciado em http://localhost:' + port)
