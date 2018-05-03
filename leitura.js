cf07e8b71f3d979ee75ac159309346466fbb1f63
RahLin182 <rafaelfclino@gmail.com>
2018-05-03 20:55:03 -0300

    bb

--- leitura.js
+++ leitura.js
@@ -1,12 +1,12 @@
 fs = require('fs')
 
-const data = fs.readFile('z.txt', 'utf8', function (err,data) {
+fs.readFile('z.txt', 'utf8', function (err,data) {
     if (err) {
       return console.log(err);
     }
-    //console.log(data);
+    console.log(data);
   });
 
 
 
-console.log(data);
+  // .split("\n") separar por linhas
