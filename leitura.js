523c90ffdf3a79848b7416fe42b914665c4c60b1
RahLin182 <rafaelfclino@gmail.com>
2018-05-04 20:14:28 -0300

    ccc

--- leitura.js
+++ leitura.js
@@ -1,8 +1,16 @@
 var fs = require('fs');
  
-var contents = fs.readFileSync('z.txt', 'utf8').split("\n");
-console.log(contents);
+var valores = 0;
+var content = fs.readFileSync('z.dat', 'utf8').split("\n");
 
+for(let i = 0; i <= content.length; i++ ){
+    valores[i] = content[i].split("\09")
+
+
+}
+//console.log(content);
+console.log(iterar)
 
 
   // .split("\n") separar por linhas
+  //size of array contents dividido por sizeof arrays para retornar o numero de casas "i"
